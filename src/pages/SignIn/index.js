fea7c959d0f494c767a359ee1734980291bc965c
tonymatheus <tony.chaves14@gmail.com>
2020-11-04 01:43:25 -0300

    Adiciona Componente de SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-
+import {Background, Container, Logo, AreaInput, Input} from './styles';
 export default function SignIn() {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <Background>
+      <Container>
+        <Logo source={require('../../assets/logoApp.png')} />
+        <AreaInput>
+          <Input 
+          placeholder="Email"
+          autoCorrect={false}
+          autoCaptalize="none"
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input 
+          placeholder="Senha"
+          autoCorrect={false}
+          autoCaptalize="none"
+          />
+        </AreaInput>
+      </Container>
+    </Background>
   );
 }
