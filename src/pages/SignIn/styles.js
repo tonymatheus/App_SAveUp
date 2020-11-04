df6849fb6c89c6940513d3dc2bd9f3c51f194b4a
tonymatheus <tony.chaves14@gmail.com>
2020-11-04 01:31:53 -0300

    Adiciona Estilizações de componentes

---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Background = styled.View``;
+
+export const Container = styled.KeyboardAvoidingView``;
+
+export const Logo = styled.Image``;
+
+export const AreaInput = styled.View``;
+export const Input = styled.TextInput``;
